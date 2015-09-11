b17a870483efc6eae64f86317f454a739a10a161
DanJarvis <Danwakeem@yahoo.com>
2015-09-11 12:54:19 -0500

    Syntax highlighting support

--- Brace-Editor/Brace-Editor-Bridging-Header.h
+++ Brace-Editor/Brace-Editor-Bridging-Header.h
@@ -2,4 +2,5 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import <AYVibrantButton/AYVibrantButton.h>
+#import "CYRTextView.h"
+#import "QEDTextView.h"
